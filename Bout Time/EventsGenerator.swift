8c01335edf59d2f4c59358ee10d698d7fd82aecd
tayalva <tay.smith@me.com>
2017-03-20 14:26:47 -0700

    added event generator. deleted all constraints, still trying to figure that out

---
+++ Bout Time/EventsGenerator.swift
@@ -0,0 +1,25 @@
+//
+//  EventsGenerator.swift
+//  Bout Time
+//
+//  Created by Taylor Smith on 3/20/17.
+//  Copyright © 2017 Taylor Smith. All rights reserved.
+//
+
+import Foundation
+
+
+struct Events {
+    let event: String
+    let year: Int
+}
+
+var events: [Events] = [
+
+Events(event: "Alaska became the 49th State", year: 1959),
+Events(event: "Space shuttle Challenger explodes during lift off", year: 1986),
+Events(event: "9/11", year: 2001),
+Events(event: "Apollo 11: Neil Armstrong is the first man to walk on the moon", year: 1969),
+Events(event: "Roe Vs. Wade: Abortions before 24 weeks are made legal", year: 1973),
+Events(event: "President Reagan challenged Gorbachev to 'Tear down this wall!'", year: 1987)
+]
